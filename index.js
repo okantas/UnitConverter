df864b4ef4b9756082dd5bfd4da1afbdb8359cd5
okantas <okantas954@gmail.com>
2023-07-05 22:16:55 +0300

    basic js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const mainNumber = document.querySelector("#main-number")
 const convertBtn = document.querySelector("#convert-btn")
+const convertLength = document.querySelector("#convert-length")
+
+let value =  mainNumber.value
 
 
 
 convertBtn.addEventListener("click", function(){
-    console.log("hey")
+    
 
 })
+
 
