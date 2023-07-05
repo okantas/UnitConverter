5f035a50d15b6d832a7ecfee3baf2f8b812e8900
okantas <okantas954@gmail.com>
2023-07-06 01:10:21 +0300

    js part is done , changed  mostly

--- index.js
+++ index.js
@@ -11,25 +11,41 @@ const conversion3 = document.querySelector("#conversion3")
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+const data=[
+    {
+        metric: "meter",
+        impariel: "feet",
+        valueOfMetric: 3.281
+    },
+    {
+        metric: "liters",
+        impariel: "gallons",
+        valueOfMetric: 0.264
+
+    },
+    {
+        metric: "kilograms",
+        impariel: "pounds",
+        valueOfMetric: 2.204
+
+    }
+
+]
+
 
 convertBtn.addEventListener("click", function(){ 
-    convert("meters","feet",3.281)
+    //convert("meters","feet",3.281)
+     let input =  mainNumber.value
+
+    conversion1.textContent = `${input} ${data[0].metric}= ${(input*data[0].valueOfMetric).toFixed(3)} ${data[0].impariel} | ${input} ${data[0].impariel} = ${(input/data[0].valueOfMetric).toFixed(3)} ${data[0].metric}`
+    conversion2.textContent = `${input} ${data[1].metric}= ${(input*data[1].valueOfMetric).toFixed(3)} ${data[1].impariel} | ${input} ${data[1].impariel} = ${(input/data[1].valueOfMetric).toFixed(3)} ${data[1].metric}`
+    conversion3.textContent = `${input} ${data[2].metric}= ${(input*data[2].valueOfMetric).toFixed(3)} ${data[2].impariel} | ${input} ${data[2].impariel} = ${(input/data[2].valueOfMetric).toFixed(3)} ${data[2].metric}`
 
 })
 
 
-function convert(metric,impariel,valueOfMetric){
-
-    let input =  mainNumber.value
-
-     let result1 = input*valueOfMetric
-     let result2 = input/valueOfMetric
-
-     let output = `${input} ${metric}= ${result1.toFixed(3)} ${impariel} | ${input} ${impariel} = ${result2.toFixed(3)} ${metric}`
-
-    conversion1.textContent = output
 
 
     
-}
+
 
