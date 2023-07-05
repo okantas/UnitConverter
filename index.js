3483da26ea0d582158704aa3f9a2304c67b0021b
okantas <okantas954@gmail.com>
2023-07-05 22:05:57 +0300

    replace p with input on html and fix the style

---
+++ index.js
@@ -0,0 +1,10 @@
+const mainNumber = document.querySelector("#main-number")
+const convertBtn = document.querySelector("#convert-btn")
+
+
+
+convertBtn.addEventListener("click", function(){
+    console.log("hey")
+
+})
+
